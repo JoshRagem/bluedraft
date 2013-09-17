f8f60019b2f43cb192e74649fbdde16b7652d2aa
PagodaJosh <pagodajosh@dongfangcanglong-Azure-Dragon>
2013-09-17 15:21:22 -0600

    getting closer!

--- test/token_test.js
+++ test/token_test.js
@@ -1,8 +1,9 @@
 var Tok = require('../lib/tokenizer/token'),
+resolve = require('path').resolve,
 fs = require('fs')
 
-stream = fs.createReadStream('./basic.blue')
+stream = fs.createReadStream(resolve(__dirname,'basic.blue'))
 t = new Tok(stream,{debug:true})
-t.on('token',function(tok){console.log('token',tok.type)})
+t.on('token',function(tok){console.log('token',tok.type,tok.value)})
 
 //console.log(t)
