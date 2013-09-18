5c8c8e22de08f1c5bd9a9f5d78667c2350f4a1df
PagodaJosh <pagodajosh@dongfangcanglong-Azure-Dragon>
2013-09-18 09:00:10 -0600

    put the debug stuff back in

--- test/token_test.js
+++ test/token_test.js
@@ -4,6 +4,8 @@ fs = require('fs')
 
 stream = fs.createReadStream(resolve(__dirname,'basic.blue'))
 t = new Tok(stream,{debug:true})
-t.on('token',function(tok){console.log('token',tok.type,tok.value)})
+t.on('token',function(tok){
+    console.log('token',tok.type,tok.value)
+})
 
 //console.log(t)
