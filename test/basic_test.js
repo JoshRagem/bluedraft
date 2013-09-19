ad70b2e8248e116da01cf66cf48bab73d26b0801
PagodaJosh <pagodajosh@dongfangcanglong-Azure-Dragon>
2013-09-19 15:02:43 -0600

    most things are working now. I need to fix the repeats so '#' accessors work, and something like '..array.#' works

--- test/basic_test.js
+++ test/basic_test.js
@@ -2,8 +2,11 @@ var Parser = require('../lib/parser/Parser'),
 
 p = new Parser({template_dir:__dirname,debug:false})
 
-p.compileFiles(['basic.blue','advanced.blue'],function(err){
-    //console.error(arguments)
+p.compileFiles(['basic.blue','advanced.blue','ultra_basic.blue'],function(err){
+    if (err) {
+        console.error(err.stack)
+        process.exit(1)
+    }
     p.Writer.writeFileWithData('basic.blue',{out:process.stdout,array:['hi'],message:'dumb',root:__dirname},function(err,stuff){
         console.error('\n\n', err?err.stack:'done')
     })
